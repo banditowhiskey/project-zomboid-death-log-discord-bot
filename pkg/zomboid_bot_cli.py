9f9e01cfa09136ebb5c31c422e649614d21caac9
Austin Harrison <systemverilogengineer@gmail.com>
2025-02-23 12:18:02 -0600

    first CLI argument added

--- pkg/zomboid_bot_cli.py
+++ pkg/zomboid_bot_cli.py
@@ -7,7 +7,27 @@ from Log import Log
 class ZomboidBotCLI(Log):
     def __init__(self):
         super().__init__(__name__)
-        self.info("poop")
+    
+    def get_cli_args(self, get_parser=False):
+        r''' @fn def get_cli_args(self, get_parsed=True)
+        @brief Gets the Command line interfaces args
+
+        @param get_parser
+        If True, it returns just the parser so that an extended class may add more arguments to the parser, if desired.
+        Else False, it returns the namespace object with the arguments parsed.
+
+        '''
+
+        parser = ArgumentParser()
+        # -n and --number_of_channels are aliased, either works
+        parser.add_argument('-n', '--number_of_channels', type=int, default=2, required=False, help="Number of Discord channels to use")
+
+        # Expected common case; common case first
+        if not get_parser:
+            return parser.parse_args()
+
+        else:
+            return parser
 
 
 def main():
@@ -18,7 +38,7 @@ def main():
     log.warn("This file defines a class to add CLI arguments to the bot, don't run this python script directly.")
     log.info("Import and instantiate or extend it else-where.")
 
-    bot = ZomboidBotCLI()
+    log.dump_status()
 
 if __name__ == "__main__":
     main()
